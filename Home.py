f007ee6c50a938c9dc02f9d271725d360b076f20
s00hyun <ff15222@gmail.com>
2024-09-08 13:00:02 +0900

    Add notebook and pages

---
+++ Home.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(page_title="Fullstack-GPT Home", page_icon="🤖")
+st.markdown(
+    """
+    # Hello!
+    
+    Welcome to my FullstackGPT Portfolio!
+
+    Here are the apps I made:
+
+    - [ ] [DocumentGPT](/DocumentGPT)
+    - [ ] [PrivateGPT](/PrivateGPT)
+    - [ ] [QuizGPT](/QuizGPT)
+    - [ ] [SiteGPT](/SiteGPT)
+    - [ ] [MeetingGPT](/MeetingGPT)
+    - [ ] [InvestorGPT](/InvestorGPT)
+"""
+)
