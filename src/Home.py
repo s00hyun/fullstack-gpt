8cc3ae80baccc7594059310d28e0af9ce5d42d34
s00hyun <ff15222@gmail.com>
2024-09-08 14:22:18 +0900

    Move source files and edit isort config

--- src/Home.py
+++ src/Home.py
@@ -4,7 +4,7 @@ st.set_page_config(page_title="Fullstack-GPT Home", page_icon="🤖")
 st.markdown(
     """
     # Hello!
-    
+
     Welcome to my FullstackGPT Portfolio!
 
     Here are the apps I made:
